ee8eb6fd6ddc7cb249b021327012f21610f43ae8
adelyagabriel <adelya.gabriel@bithealth.co.id>
2024-01-18 17:06:25 +0700

    add assignment 4

---
+++ assignment4.js
@@ -0,0 +1,41 @@
+const getData = async() => {
+    // console.log(e, "==> E");
+    // e.preventDefault();
+    try {
+        let inputan = document.getElementById("inputCountry").value;
+        const fetchUrl = await fetch(`https://covid-193.p.rapidapi.com/statistics?country=${inputan}`, {
+            headers: {
+                'X-RapidAPI-Key': '772cdf7666msh0b4026c3343f42ep15dcedjsn50bc4ebdcb90',
+                'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+            }
+        })
+
+        // console.log(inputan);
+        const dataJson = await fetchUrl.json();
+        const data = dataJson.response[0];
+        console.log(data);
+
+        let activeCases = ifNull(data.cases.active)
+        let newCases = ifNull(data.cases.new)
+        let recoveredCases = ifNull(data.cases.recovered)
+        let totalDeaths = ifNull(data.deaths.total)
+        let totalTests = ifNull(data.tests.total)
+        let totalCases = ifNull(data.cases.total)
+        
+        document.getElementById("activeCases").innerHTML = activeCases;
+        document.getElementById("newCases").innerHTML = newCases;
+        document.getElementById("recoveredCases").innerHTML = recoveredCases;
+        document.getElementById("totalDeaths").innerHTML = totalDeaths;
+        document.getElementById("totalTests").innerHTML = totalTests;
+        document.getElementById("totalCases").innerHTML = totalCases;
+
+    } catch (err) {
+        console.log(err);
+    }
+
+    console.log("HALO");
+}
+
+function ifNull(num) {
+    return num ?? 0;
+}
